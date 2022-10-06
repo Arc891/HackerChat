8bebc584f535809ee6450967f89e8a9c61ce6d0a
Arc891 <ezra.hulsman@gmail.com>
2022-10-06 11:57:58 +0200

    Split run_login and made function comments work

--- curses_test.py
+++ curses_test.py
@@ -39,11 +39,10 @@ DELIVERY = "DELIVERY".encode("utf-8")
 #         return helper
 #     return increment_lines
 
-"""
-Custom print function in style of the terminal onto a specific screen, 
-taking a 3rd parameter which defines the icon between the square brackets. 
-"""
 def cprint(screen, x=0, y=0, text="", pre=INF):
+    """Custom print function in style of the terminal onto a specific screen, 
+    taking a 3rd parameter which defines the icon between the square brackets. """
+
     global LINES
     screen.addstr(y, x, f"[{pre}] {text}")
     screen.refresh()
@@ -51,12 +50,11 @@ def cprint(screen, x=0, y=0, text="", pre=INF):
     return
 
 
-"""
-Custom input function in style of the terminal in a specific screen,
-taking a 3rd parameter checking if the input is going to be a password,
-if so, the input will be hidden while typed.
-"""
 def cinput(screen, x=0, y=0, text="", pwd=False):
+    """Custom input function in style of the terminal in a specific screen,
+    taking a 3rd parameter checking if the input is going to be a password,
+    if so, the input will be hidden while typed."""
+    
     global LINES
     if pwd: curses.noecho()
     cprint(screen, x, y, text, INP)
@@ -65,18 +63,16 @@ def cinput(screen, x=0, y=0, text="", pwd=False):
     if y == LINES: LINES += 1
     return inp.decode('utf-8')
 
-"""
-Quick constructor for new messages to send to the server
-"""
 def new_msg(sender, msg_type, content="", receiver=None):
+    """Quick constructor for new messages to sent to the server"""
+
     return Message(sender, msg_type, content=content, receiver=receiver).to_json().encode('utf-8')
 
 
-"""
-Will be ran by a thread keeping check of the terminal size
-Will resize the screens if it does change
-"""
 def check_screen_size(stdscr):
+    """To be ran by a thread keeping check of the terminal size.
+    Will resize the screens if it does change"""
+
     global HEIGHT, WIDTH
     while True:
         w, h = os.get_terminal_size()
@@ -85,27 +81,25 @@ def check_screen_size(stdscr):
 
 
 
-"""
-Sets the sizes of all the pads and screens based on the current terminal size
-"""
 def set_sizes(stdscr):
+    """Sets the size values of all the pads and screens based on the current terminal size"""
+
     global HEIGHT, WIDTH, IS_HEIGHT, IS_WIDTH, OI_HEIGHT, OI_WIDTH, II_HEIGHT, II_WIDTH
     HEIGHT, WIDTH = stdscr.getmaxyx()
     IS_HEIGHT = HEIGHT - 2
-    IS_WIDTH = WIDTH - 4
+    IS_WIDTH  = WIDTH - 4
     OI_HEIGHT = HEIGHT // 6
-    OI_WIDTH = WIDTH - 4
+    OI_WIDTH  = WIDTH - 4
     II_HEIGHT = OI_HEIGHT - 2
-    II_WIDTH = OI_WIDTH - 2
+    II_WIDTH  = OI_WIDTH - 2
 
     IS_HEIGHT -= OI_HEIGHT
     return
 
 
-"""
-Creates the physical screens that the application will be made out of
-"""
 def create_screens(stdscr):
+    """Creates the physical screens that the application will be made out of"""
+
     global HEIGHT, WIDTH, IS_HEIGHT, IS_WIDTH, OI_HEIGHT, OI_WIDTH, II_HEIGHT, II_WIDTH
     global screen_inner, input_outer, input_inner
     curses.echo()
@@ -119,32 +113,32 @@ def create_screens(stdscr):
 
 
 
-"""
-Sets up screens by clearing and adding borders etc 
-"""
-def setup_login_screen(stdscr) -> int:
-    global HEIGHT, WIDTH, screen_inner
+def setup_login_screen(stdscr):
+    """Sets up screens by clearing and adding borders etc"""
+
+    global HEIGHT, WIDTH, LINES, screen_inner
 
     stdscr.clear()
     screen_inner.clear()
 
     stdscr.border("|", "|", "-", "-", "+", "+", "+", "+")
     
-    curr_lines = 0
+    LINES = 0
 
     with open('Logos/logo-full-width.txt', 'r') as f:
         for i, line in enumerate(f):
-            screen_inner.addstr(i, (WIDTH-len(line))//2-1, line[:-1])
-            curr_lines += 1 
+            screen_inner.addstr(i, (WIDTH-len(line))//2-1, line[:-1], curses.A_BOLD)
+            LINES += 1 
 
-    screen_inner.addstr(curr_lines,0, "-"*(WIDTH-4))
-
+    screen_inner.addstr(LINES,0, "-"*(WIDTH-4))
+    LINES += 1
     stdscr.refresh()
     screen_inner.refresh()
-    return curr_lines+1
+    return
 
+def get_login_credentials():
+    """Gather login credentials and return them in a Message to run with run_login"""
 
-def run_login():
     global SIGNUP, LINES, screen_inner
     
     while True:
@@ -178,14 +172,18 @@ def run_login():
     pwd = sha256(pwd.encode("utf-8")).hexdigest()
 
     # remember = cinput("Do you want to remember your login details? (y/n): ")
-    
-    u = User(user, pwd) 
-    msg_type = "SIGNUP" if SIGNUP else "LOGIN"
-    message2 = Message(u, msg_type)
-    # print(s)
-    # print(message2.to_json())
 
-    string_bytes = message2.to_json().encode("utf-8")
+    msg_type = "SIGNUP" if SIGNUP else "LOGIN"
+    return Message(User(user, pwd), msg_type)
+    
+def run_login(msg: Message):
+    """Connects with the server and tries to login with the given credentials.\n
+    Also handles any errors that might be returned when attempting login."""
+
+    global SIGNUP, LINES, screen_inner
+
+    user = msg.sender
+    string_bytes = msg.to_json().encode("utf-8")
 
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -206,40 +204,44 @@ def run_login():
         decoded_data = data.decode("utf-8")
 
         try:
-            msg = Message(**json.loads(decoded_data))
+            response = Message(**json.loads(decoded_data))
         except json.decoder.JSONDecodeError:
             pass
         finally:
-            if msg.message_type == "HELLO":
+            if response.message_type == "HELLO":
                 string_bytes = b''
                 break
 
             else:
                 s.close()
-                if msg.message_type == "BAD-PASS":
+                if response.message_type == "BAD-PASS":
                     pwd = cinput(screen_inner, 0, LINES, 'Password is incorrect, try again: ', pwd=True)
-                    u.password = sha256(pwd.encode("utf-8")).hexdigest()
-                    string_bytes = new_msg(u, msg_type)
+                    user.password = sha256(pwd.encode("utf-8")).hexdigest()
+                    string_bytes = new_msg(user, msg.msg_type)
                     continue
 
                 err_msg = ""
-                if msg.message_type == "BAD-RQST-BODY": err_msg = "invalid"
-                elif msg.message_type == "UNKNOWN":     err_msg = "not known"
-                else:                                   err_msg = "taken"
-                user = cinput(screen_inner, 0, LINES, f'Username is {err_msg}, please enter another: ')
-                u.username = user
-                string_bytes = new_msg(u, msg_type)
+                if response.message_type == "BAD-RQST-BODY": err_msg = "invalid"
+                elif response.message_type == "UNKNOWN":     err_msg = "not known"
+                else:                                        err_msg = "taken"
+                username = cinput(screen_inner, 0, LINES, f'Username is {err_msg}, please enter another: ')
+                user.name = username
+                string_bytes = new_msg(user, msg.msg_type)
     
     LINES += 1
-    cprint(screen_inner, 0, LINES, f"Welcome {user}!", SUC)
+    cprint(screen_inner, 0, LINES, f"Welcome {user.name}!", SUC)
     # print home screen and initiate respective functions
+    return True
 
 
-"""
-Sets up screens by clearing and adding borders etc 
-"""
-def setup_screens(stdscr):
-    global HEIGHT, WIDTH, screen_inner, input_outer, input_inner
+def run_main():
+    pass
+
+
+def setup_main_screen(stdscr):
+    """Sets up screens by clearing and adding borders etc"""
+
+    global HEIGHT, WIDTH, LINES, screen_inner, input_outer, input_inner
 
     stdscr.clear()
     screen_inner.clear()
@@ -249,11 +251,15 @@ def setup_screens(stdscr):
     stdscr.border("|", "|", "-", "-", "+", "+", "+", "+")
     input_outer.border("|", "|", "-", "-", "+", "+", "+", "+")
     
+    LINES = 0
+
     with open('Logos/logo-full-width.txt', 'r') as f:
         for i, line in enumerate(f):
-            screen_inner.addstr(i, (WIDTH-len(line))//2-1, line[:-1])
+            screen_inner.addstr(i, (WIDTH-len(line))//2-1, line[:-1], curses.A_BOLD)
+            LINES += 1
 
-    screen_inner.addstr(HEIGHT//2-1,0, "-"*(WIDTH-5))
+    screen_inner.addstr(LINES,0, "-"*(WIDTH-5))
+    LINES += 1
 
     stdscr.refresh()
     screen_inner.refresh()
@@ -262,46 +268,32 @@ def setup_screens(stdscr):
     return
 
 
-"""
-Resizes the screens and prints the new layout
-"""
 def resize_and_setup(stdscr):
+    """Resizes the screens and prints the new layout"""
+
     set_sizes(stdscr)
 
     screen_inner.resize(IS_HEIGHT, IS_WIDTH)
     input_outer.resize(OI_HEIGHT, OI_WIDTH)
     input_inner.resize(II_HEIGHT, II_WIDTH)
 
-    setup_screens(stdscr)
+    setup_main_screen(stdscr)
     return
 
 
-"""
-Main function
-"""
 def main(stdscr):
+    """Main function"""
+
     global HEIGHT, WIDTH, LINES, screen_inner, input_outer, input_inner    
     create_screens(stdscr)
-    # setup_screens(stdscr)
-    LINES = setup_login_screen(stdscr)
-    run_login()
+    setup_login_screen(stdscr)
+    login = get_login_credentials()
+    run_login(login)
+    setup_main_screen(stdscr)
+    run_main()
     # t = threading.Thread(target=check_screen_size, args=(stdscr,), daemon=True)
     # t.start()
     stdscr.getch()
     return
-    line = 0
-    while True:
-        inp = input_inner.getstr(0, 0).decode('utf-8')
-        
-        if inp == "!q":
-            break
-        
-        cprint(screen_inner, inp, INP, 0, line)
-        input_inner.clear()
-
-        screen_inner.refresh()
-        input_inner.refresh() 
-        line += 1
-    
 
 wrapper(main)
