ee90d62eba4ed6581522fd1acff616128d7a507b
Arc891 <ezra.hulsman@gmail.com>
2022-10-15 18:56:38 +0200

    Added exceptions.py with all custom exception classes

---
+++ Classes/exceptions.py
@@ -0,0 +1,24 @@
+class BAD_RQST_HDR(Exception):
+    def __init__(self):
+        pass
+
+class BAD_RQST_BODY(Exception):
+    def __init__(self):
+        pass
+
+class BUSY(Exception):
+    def __init__(self):
+        pass
+
+class IN_USE(Exception):
+    def __init__(self):
+        pass
+
+class BAD_PASS(Exception):
+    def __init__(self):
+        pass
+
+class UNKNOWN(Exception):
+    def __init__(self):
+        pass
+
